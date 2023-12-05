7be1736d87e19870437f1c55600da098af884d16
behos <behos@users.noreply.github.com>
2023-12-05 12:54:12 +0000

    deploy: fb3dcba5693ad68975dab2440a1650e468e620a6

---
+++ crates.js
@@ -0,0 +1,1 @@
+window.ALL_CRATES = ["bindingtester","fdb_bench","foundationdb","foundationdb_bench","foundationdb_gen","foundationdb_macros","foundationdb_options_gen","foundationdb_simulation","foundationdb_sys"];
---
+++ implementors/structopt/trait.StructOpt.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"fdb_bench":[["impl StructOpt for <a class=\"struct\" href=\"fdb_bench/struct.Opt.html\" title=\"struct fdb_bench::Opt\">Opt</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
